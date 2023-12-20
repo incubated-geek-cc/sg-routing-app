e8ef3477f0b487db61ce9b67e8e92f65038c9134
incubated-geek-cc <xuemanxu.cc@gmail.com>
2023-12-20 21:59:37 +0800

    Set access token as bearer token for authorization. updated onemap apis

--- app.js
+++ app.js
@@ -18,18 +18,31 @@ const favicon = require("serve-favicon");
 const engine = require("consolidate");
 const compression = require('compression');
 
+var ONEMAP_API_TOKEN = ""; // Onemap API Token
+request({
+    url: "https://www.onemap.gov.sg/api/auth/post/getToken",
+    // https://developers.onemap.sg/privateapi/auth/post/getToken
+    method: "POST",
+    json: true,
+    body: {
+      email: ONEMAP_EMAIL,
+      password: ONEMAP_PASSWORD
+    }
+}, (err, res, body) => {
+  ONEMAP_API_TOKEN = body.access_token;
+});
+
 // set up router
 var router = express.Router();
-router.use(
-  express.urlencoded({
-    extended: true
-  })
-)
-router.use(express.json())
+router.use(express.urlencoded({ extended: true}));
+router.use(express.json());
 router.use((req, res, next) => { // router middleware
-    res.header("Access-Control-Allow-Origin", process.env.ORIGIN || "*");
+    res.setHeader('Access-Control-Allow-Origin', process.env.ORIGIN || '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST');
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type, Authorization');
     next();
 });
+
 
 const app = express();
 // Compress all HTTP responses
@@ -48,19 +61,6 @@ app.use(express.static(path.join(__dirname, "public")))
 .listen(PORT, () => {
   console.log(`SG Transportation Routing App [using Proxy] is listening on port ${PORT}!`)
   // require("openurl").open(`http://localhost:${PORT}/index.html`)
-});
-
-var ONEMAP_API_TOKEN = ""; // Onemap API Token
-request({
-    url: "https://developers.onemap.sg/privateapi/auth/post/getToken",
-    method: "POST",
-    json: true,
-    body: {
-      email: ONEMAP_EMAIL,
-      password: ONEMAP_PASSWORD
-    }
-}, (err, res, body) => {
-  ONEMAP_API_TOKEN = body.access_token;
 });
 
 function concatParams(baseUrl, params={}) {
@@ -93,16 +93,26 @@ router.get("/opencage/geocode/json/v1/:language/:q", (req, res) => {
     res.json(JSON.parse(body))
   })
 });
-
+// eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiI5ZTU2ZDg0MDEzODk3MzdjY2U3NjVmMGUzMGIzMTgyMSIsImlzcyI6Imh0dHA6Ly9pbnRlcm5hbC1hbGItb20tcHJkZXppdC1pdC0xMjIzNjk4OTkyLmFwLXNvdXRoZWFzdC0xLmVsYi5hbWF6b25hd3MuY29tL2FwaS92Mi91c2VyL3Bhc3N3b3JkIiwiaWF0IjoxNzAzMDc4MDk0LCJleHAiOjE3MDMzMzcyOTQsIm5iZiI6MTcwMzA3ODA5NCwianRpIjoiTEpvR0hNN1JHaGswQ1hmYSIsInVzZXJfaWQiOjIwMzksImZvcmV2ZXIiOmZhbHNlfQ.hfbw-hPLQXKcsV7Hl-0vqEcw4BfoUv75bpT2ZEP5hoo
 
 // Onemap Routing API
 router.get("/onemap/directions/json/:routeType/:start/:end", (req, res) => {   
-  let baseUrl="https://developers.onemap.sg/privateapi/routingsvc/route?"; // call onemap routing api via a proxy
+  let baseUrl="https://www.onemap.gov.sg/api/public/routingsvc/route?"; // call onemap routing api via a proxy
+  // https://www.onemap.gov.sg/api/public/routingsvc/route?start=1.319728%2C103.8421&end=1.319728905%2C103.8421581&routeType=walk
+  // https://developers.onemap.sg/privateapi/routingsvc/route
   let params=req.params;
   params["token"]=ONEMAP_API_TOKEN;
 
   let fullUrl=concatParams(baseUrl,params);
-  request({ url: fullUrl }, (err, response, body) => {
+
+  request({ 
+    url: fullUrl,
+    headers: {
+      "Accept": "application/json",
+      "Content-Type": "application/json",
+      "Authorization": `Bearer ${ONEMAP_API_TOKEN}`
+    }
+  }, (err, response, body) => {
     // console.log(body);
     let immediateResponse=JSON.parse(body);
     if(typeof immediateResponse["error"] !== "undefined") {
@@ -119,6 +129,27 @@ router.get("/onemap/directions/json/:routeType/:start/:end", (req, res) => {
     res.json(JSON.parse(body))
   })
 });
+
+
+// let options={
+//   url: endpoint,
+//   headers: {
+//     "Accept": "application/json",
+//     "Content-Type": "application/json"
+//   },
+//   body: JSON.stringify(postData)
+// };
+// request.post(options, (err, res, body) => {
+//   let result=res.body;
+//   if (err) {
+//     return _res.status(500).json({ 
+//       type: "error", 
+//       message: (err !== null && typeof err.message !== "undefined") ? err.message : "Error."
+//     });
+//   }
+//   _res.json(JSON.parse(result));
+// })
+
 
 router.get("/graphhopper/route/json/:locale/:point_o/:point_d/:elevation/:profile", (req, res) => {
   let baseUrl="https://graphhopper.com/api/1/route?";
@@ -158,8 +189,4 @@ router.get("/hereapi/v8/route/json/:origin/:destination/:transportMode", (req, r
     }
     res.json(JSON.parse(body))
   })
-});
-
-router.get("/wake_up", (req, res) => {
-  res.json({"status":"app_is_awake"});
 });
