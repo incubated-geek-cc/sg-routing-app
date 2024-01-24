ceac94002ed0ab87438e190edbae0785da2ef80d
incubated-geek-cc <xuemanxu.cc@gmail.com>
2024-01-25 07:28:00 +0800

    Replace HERE API TOKEN with a BACKUP TOKEN instead.

--- app.js
+++ app.js
@@ -11,6 +11,7 @@ const ONEMAP_PASSWORD = process.env.ONEMAP_PASSWORD;
 
 const GRAPHHOPPER_API_KEY=process.env.GRAPHHOPPER_API_KEY;
 const HERE_API_KEY=process.env.HERE_API_KEY;
+const HERE_API_KEY_BACKUP=process.env.HERE_API_KEY;
 
 const path = require("path");
 const request = require("request");
@@ -18,21 +19,21 @@ const favicon = require("serve-favicon");
 const engine = require("consolidate");
 const compression = require('compression');
 
-var ONEMAP_API_TOKEN = ""; // Onemap API Token
-request({
-    url: "https://www.onemap.gov.sg/api/auth/post/getToken",
-    // https://developers.onemap.sg/privateapi/auth/post/getToken
-    method: "POST",
+const authOptions={
+    url: 'https://www.onemap.gov.sg/api/auth/post/getToken',
+    method: 'POST',
     json: true,
     body: {
       email: ONEMAP_EMAIL,
       password: ONEMAP_PASSWORD
     }
-}, (err, res, body) => {
+};
+var ONEMAP_API_TOKEN = ""; // Onemap API Token
+request(authOptions, (err, res, body) => {
+  // console.log(body.access_token);
   ONEMAP_API_TOKEN = body.access_token;
 });
 
-// set up router
 var router = express.Router();
 router.use(express.urlencoded({ extended: true}));
 router.use(express.json());
@@ -42,7 +43,6 @@ router.use((req, res, next) => { // router middleware
     res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type, Authorization');
     next();
 });
-
 
 const app = express();
 // Compress all HTTP responses
@@ -93,7 +93,6 @@ router.get("/opencage/geocode/json/v1/:language/:q", (req, res) => {
     res.json(JSON.parse(body))
   })
 });
-// eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiI5ZTU2ZDg0MDEzODk3MzdjY2U3NjVmMGUzMGIzMTgyMSIsImlzcyI6Imh0dHA6Ly9pbnRlcm5hbC1hbGItb20tcHJkZXppdC1pdC0xMjIzNjk4OTkyLmFwLXNvdXRoZWFzdC0xLmVsYi5hbWF6b25hd3MuY29tL2FwaS92Mi91c2VyL3Bhc3N3b3JkIiwiaWF0IjoxNzAzMDc4MDk0LCJleHAiOjE3MDMzMzcyOTQsIm5iZiI6MTcwMzA3ODA5NCwianRpIjoiTEpvR0hNN1JHaGswQ1hmYSIsInVzZXJfaWQiOjIwMzksImZvcmV2ZXIiOmZhbHNlfQ.hfbw-hPLQXKcsV7Hl-0vqEcw4BfoUv75bpT2ZEP5hoo
 
 // Onemap Routing API
 router.get("/onemap/directions/json/:routeType/:start/:end", (req, res) => {   
@@ -101,8 +100,6 @@ router.get("/onemap/directions/json/:routeType/:start/:end", (req, res) => {
   // https://www.onemap.gov.sg/api/public/routingsvc/route?start=1.319728%2C103.8421&end=1.319728905%2C103.8421581&routeType=walk
   // https://developers.onemap.sg/privateapi/routingsvc/route
   let params=req.params;
-  params["token"]=ONEMAP_API_TOKEN;
-
   let fullUrl=concatParams(baseUrl,params);
 
   request({ 
@@ -130,27 +127,6 @@ router.get("/onemap/directions/json/:routeType/:start/:end", (req, res) => {
   })
 });
 
-
-// let options={
-//   url: endpoint,
-//   headers: {
-//     "Accept": "application/json",
-//     "Content-Type": "application/json"
-//   },
-//   body: JSON.stringify(postData)
-// };
-// request.post(options, (err, res, body) => {
-//   let result=res.body;
-//   if (err) {
-//     return _res.status(500).json({ 
-//       type: "error", 
-//       message: (err !== null && typeof err.message !== "undefined") ? err.message : "Error."
-//     });
-//   }
-//   _res.json(JSON.parse(result));
-// })
-
-
 router.get("/graphhopper/route/json/:locale/:point_o/:point_d/:elevation/:profile", (req, res) => {
   let baseUrl="https://graphhopper.com/api/1/route?";
   let params=req.params;
@@ -172,11 +148,13 @@ router.get("/graphhopper/route/json/:locale/:point_o/:point_d/:elevation/:profil
   })
 });
 
+
+// https://router.hereapi.com/v8/routes?transportMode=car&origin=52.5308,13.3847&destination=52.5264,13.3686&return=summary&apikey={YOUR_API_KEY}
 router.get("/hereapi/v8/route/json/:origin/:destination/:transportMode", (req, res) => {
   let baseUrl="https://router.hereapi.com/v8/routes?";
   let params=req.params;
   params["return"]="polyline,actions,instructions,travelSummary";
-  params["apikey"]=HERE_API_KEY;
+  params["apikey"]=HERE_API_KEY_BACKUP;
 
   let fullUrl=concatParams(baseUrl,params);
 
